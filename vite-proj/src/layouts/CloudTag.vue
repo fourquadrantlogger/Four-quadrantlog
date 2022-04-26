184be8ea51494d0b5fff709560c475e889276e89
timeloveboy <734991033@qq.com>
2022-04-26 14:53:03 +0800

    feat(vite-proj):cloudtag four quadrant

--- vite-proj/src/layouts/CloudTag.vue
+++ vite-proj/src/layouts/CloudTag.vue
@@ -1,6 +1,5 @@
 <template>
-    <div id="mywordcloud" :style="{ width: '100%', height: '600px' }" :data="worddata"></div>
-
+<div v-html="cdiv+cid+cdiv_"></div>
 </template>
 <script lang="javascript">
 
@@ -8,39 +7,82 @@ import * as echarts from "echarts";
 import "echarts-wordcloud/dist/echarts-wordcloud";
 import "echarts-wordcloud/dist/echarts-wordcloud.min";
 
-import { getTagList, Quadrant } from '../apis/apis'
-
-import { ElMessage } from 'element-plus'
 
 export default {
     name: "VueWordCloud",
-    data() {
+    data: function () {
         return {
-            msg: 'Welcome to Your Vue.js App',
-            worddata: [
-
-            ],
+            cdiv: ` <div id="`,
+            cid: Math.random().toString(36).slice(-6),
+            cdiv_: `" style="width:100%;height:100%;"></div>`,
         }
     },
-
-    mounted() {
-        this.tagLog().then(
-            x => {
-                this.initChart();
-            }
-        )  //  输出结果 1， 1，
-
-        this.tagLog();
-
+    props: {
+        datas: [],
+        title:'',
+    },
+    watch: {
+        datas: { //深度监听，可监听到对象、数组的变化
+            handler(newV, oldV) {
+                // do something, 可使用this
+                console.log(newV, oldV)
+                this.initChart()
+            },
+            deep: true
+        },
     },
     methods: {
         initChart() {
             var tagcom = this
-            console.log("tagcom.worddata", tagcom.$data.worddata)
-            var myChart = echarts.init(document.getElementById("mywordcloud"));
-            const option = {
+            console.log("datas", tagcom.$props.datas)
+            var chartquadrant = echarts.init(document.getElementById(tagcom.$data.cid));
+
+            var series = {
+                type: "wordCloud",
+                //用来调整词之间的距离
+                gridSize: 10,
+                //用来调整字的大小范围
+                // Text size range which the value in data will be mapped to.
+                // Default to have minimum 12px and maximum 60px size.
+                sizeRange: [14, 60],
+                // Text rotation range and step in degree. Text will be rotated randomly in range [-90,                                                                             90] by rotationStep 45
+                //用来调整词的旋转方向，，[0,0]--代表着没有角度，也就是词为水平方向，需要设置角度参考注释内容
+                // rotationRange: [-45, 0, 45, 90],
+                // rotationRange: [ 0,90],
+                rotationRange: [0, 0],
+                //随机生成字体颜色
+                // maskImage: maskImage,
+                textStyle: {
+                    normal: {
+                        color: function () {
+                            return (
+                                "rgb(" +
+                                Math.round(Math.random() * 160) +
+                                ", " +
+                                Math.round(Math.random() * 160) +
+                                ", " +
+                                Math.round(Math.random() * 160) +
+                                ")"
+                            );
+                        }
+                    }
+                },
+                //位置相关设置
+                // Folllowing left/top/width/height/right/bottom are used for positioning the word cloud
+                // Default to be put in the center and has 75% x 80% size.
+                left: "center",
+                top: "center",
+                right: null,
+                bottom: null,
+                width: "200%",
+                height: "200%",
+                //数据
+                data: tagcom.$props.datas,
+            }
+
+            let option = {
                 title: {
-                    text: "词云",
+                    text: tagcom.$data.title,
                     x: "center"
                 },
                 backgroundColor: "#fff",
@@ -48,101 +90,23 @@ export default {
                 //   pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>"
                 // },
                 series: [
-                    {
-                        type: "wordCloud",
-                        //用来调整词之间的距离
-                        gridSize: 10,
-                        //用来调整字的大小范围
-                        // Text size range which the value in data will be mapped to.
-                        // Default to have minimum 12px and maximum 60px size.
-                        sizeRange: [14, 60],
-                        // Text rotation range and step in degree. Text will be rotated randomly in range [-90,                                                                             90] by rotationStep 45
-                        //用来调整词的旋转方向，，[0,0]--代表着没有角度，也就是词为水平方向，需要设置角度参考注释内容
-                        // rotationRange: [-45, 0, 45, 90],
-                        // rotationRange: [ 0,90],
-                        rotationRange: [0, 0],
-                        //随机生成字体颜色
-                        // maskImage: maskImage,
-                        textStyle: {
-                            normal: {
-                                color: function () {
-                                    return (
-                                        "rgb(" +
-                                        Math.round(Math.random() * 255) +
-                                        ", " +
-                                        Math.round(Math.random() * 255) +
-                                        ", " +
-                                        Math.round(Math.random() * 255) +
-                                        ")"
-                                    );
-                                }
-                            }
-                        },
-                        //位置相关设置
-                        // Folllowing left/top/width/height/right/bottom are used for positioning the word cloud
-                        // Default to be put in the center and has 75% x 80% size.
-                        left: "center",
-                        top: "center",
-                        right: null,
-                        bottom: null,
-                        width: "200%",
-                        height: "200%",
-                        //数据
-                        data: tagcom.$data.worddata
-                    }
+                    series
                 ]
             };
-            console.log("tagcom.worddata", tagcom.$data.worddata)
-            myChart.setOption(option);
-            myChart.on('click', function (ps) {
+
+
+            chartquadrant.setOption(option);
+
+            var chartclick = function (ps) {
                 console.log('myChart----click---:', ps, '------', ps.data)
                 tagcom.$router.push({
-                    path:"/table",
-                    query:{
-                        'atypequery':ps.data.name,
+                    path: "/table",
+                    query: {
+                        'atypequery': ps.data.name,
                     }
                 })
-            });
-        },
-        async tagLog() {
-            let query = {}
-            query.limit = 40
-            
-            let start = new Date()
-            start.setMonth (start.getMonth ()-3);
-            query.start=start.toLocaleString()
-            // if (this.ctimestartquery != null) {
-            //     query.start = this.ctimestartquery.toLocaleString()
-            // }
-            // if (this.ctimeendquery != null) {
-            //     query.end = this.ctimeendquery.toLocaleString()
-            // }
-            // if (this.quadrantquery != null) {
-            //     if (this.quadrantquery instanceof Array) {
-            //         query.quadrant = this.quadrantquery.join('/')
-            //     } else {
-            //         query.quadrant = this.quadrantquery
-            //     }
-
-            // }
-
-            // if (this.locationquery != undefined) {
-            //     query.location = this.locationquery
-            // }
-            // if (this.titlequery != undefined) {
-            //     query.title = this.titlequery
-            // }
-            // if (this.detailquery != undefined) {
-            //     query.detail = this.detailquery
-            // } if (this.reviewquery != undefined) {
-            //     query.review = this.reviewquery
-            // }
-
-
-            const taglist = await getTagList(query)
-            this.worddata = [].concat(taglist)
-            ElMessage.success('获取tags成功')
-            return
+            }
+            chartquadrant.on('click', chartclick);
         },
     }
 }
--- vite-proj/src/pages/Dashboard.vue
+++ vite-proj/src/pages/Dashboard.vue
@@ -1,16 +1,93 @@
 <template>
-    <CloudTag style="width:500px;height:400px;">
-
-    </CloudTag>
+<div style="display: flex " class="all">
+   <CloudTag style="width:400px;height:400px;" :datas="q1" :title='肉体象限'></CloudTag>
+  <CloudTag style="width:400px;height:400px;" :datas="q2" :title='知识信息象限'></CloudTag>
+  <CloudTag style="width:400px;height:400px;" :datas="q3" :title='社会关系象限'></CloudTag>
+  <CloudTag style="width:400px;height:500px;" :datas="q4" :title='持有物象限'></CloudTag>
+</div>
 
 </template>
-<style >
-
+<style lang="css">
 </style>
 <script>
 import CloudTag from '../layouts/CloudTag.vue'
+import { getTagList, Quadrant } from '../apis/apis'
+
+import { ElMessage } from 'element-plus'
+
 export default {
+
   components: { CloudTag },
-    
+  data: function () {
+    return {
+      q1: [],
+      q2: [],
+      q3: [],
+      q4: [],
+    }
+
+  },
+  mounted: function () {
+    this.tagLog()
+  },
+  methods: {
+    async tagLog() {
+      let query = {}
+      query.limit = 50
+      let start = new Date()
+      start.setMonth(start.getMonth() - 3);
+      query.start = start.toLocaleString()
+      // if (this.ctimestartquery != null) {
+      //     query.start = this.ctimestartquery.toLocaleString()
+      // }
+      // if (this.ctimeendquery != null) {
+      //     query.end = this.ctimeendquery.toLocaleString()
+      // }
+      // if (this.quadrantquery != null) {
+      //     if (this.quadrantquery instanceof Array) {
+      //         query.quadrant = this.quadrantquery.join('/')
+      //     } else {
+      //         query.quadrant = this.quadrantquery
+      //     }
+
+      // }
+
+      // if (this.locationquery != undefined) {
+      //     query.location = this.locationquery
+      // }
+      // if (this.titlequery != undefined) {
+      //     query.title = this.titlequery
+      // }
+      // if (this.detailquery != undefined) {
+      //     query.detail = this.detailquery
+      // } if (this.reviewquery != undefined) {
+      //     query.review = this.reviewquery
+      // }
+
+      {
+        query.quadrant = '肉体'
+        const taglist = await getTagList(query)
+        this.q1 = [].concat(taglist)
+      }
+      {
+        query.quadrant = '信息'
+        const taglist = await getTagList(query)
+        this.q2 = [].concat(taglist)
+      }
+      {
+        query.quadrant = '社会关系'
+        const taglist = await getTagList(query)
+        this.q3 = [].concat(taglist)
+      }
+      {
+        query.quadrant = '持有物'
+        const taglist = await getTagList(query)
+        this.q4 = [].concat(taglist)
+      }
+
+      ElMessage.success('获取tags成功')
+      return
+    },
+  },
 }
 </script>
